c41acbfea42b1ca327c5680b6ef6d25f51eef31d
Gianluca Milano <gm@gianlucamilano.com>
2023-04-18 10:00:31 +0200

    first commit

---
+++ l10n_it_account_banking_sepa_direct_debit/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Banking SEPA Direct Debit for Italy",
+    "summary": "Create SEPA files for Direct Debit",
+    "version": "14.0.0.0.1",
+    "license": "AGPL-3",
+    "author": "Protom, berim, Ooops404, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "category": "Banking addons",
+    "external_dependencies": {"python": ["fintech>=7.2.10"]},
+    "depends": ["account_banking_sepa_direct_debit"],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
